0c28174d14391b989237bf9e625d86e71f2261e3
Ivan Kardapoltsev <98235510+Pisex@users.noreply.github.com>
2025-04-02 11:26:38 +0500

    1.0

---
+++ auto_map_delete.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <algorithm>
+
+#include "auto_map_delete.h"
+#include "metamod_oslink.h"
+
+AutoMapDelete g_AutoMapDelete;
+PLUGIN_EXPOSE(AutoMapDelete, g_AutoMapDelete);
+
+IVEngineServer2* engine = nullptr;
+
+bool AutoMapDelete::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late)
+{
+	PLUGIN_SAVEVARS();
+    GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer2, SOURCE2ENGINETOSERVER_INTERFACE_VERSION);
+
+    g_SMAPI->AddListener(this, this);
+
+	char szPath[256];
+	g_SMAPI->PathFormat(szPath, sizeof(szPath), "%s/addons/configs/AutoClearMaps/whitelist.txt", g_SMAPI->GetBaseDir());
+
+	if (!std::filesystem::exists(szPath))
+	{
+		Msg("[AutoClearMaps v1.0.0] Ошибка: whitelist.txt не найден \n");
+		Msg("[AutoClearMaps v1.0.0] Ошибка: whitelist.txt не найден \n");
+		Msg("[AutoClearMaps v1.0.0] Ошибка: whitelist.txt не найден \n");
+		Msg("[AutoClearMaps v1.0.0] Ошибка: whitelist.txt не найден \n");
+		Msg("[AutoClearMaps v1.0.0] Ошибка: whitelist.txt не найден \n");
+		return false;
+	}
+
+	std::vector<std::string> whitelist;
+	std::ifstream infile(szPath);
+	std::string line;
+	
+	while (std::getline(infile, line))
+	{
+		line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
+	
+		if (!line.empty()) {
+			Msg("[AutoClearMaps v1.0.0] whitelist: %s\n", line.c_str());
+			whitelist.push_back(line);
+		}
+	}
+
+	char szPathMaps[256];
+	g_SMAPI->PathFormat(szPathMaps, sizeof(szPathMaps), "%s/maps", g_SMAPI->GetBaseDir());
+	DeleteMaps(szPathMaps, whitelist);
+	
+    return true;
+}
+
+bool AutoMapDelete::Unload(char* error, size_t maxlen)
+{
+    return true;
+}
+
+void AutoMapDelete::DeleteMaps(const std::string& folderPath, const std::vector<std::string>& whitelist)
+{
+    try
+    {
+        for (const auto& entry : std::filesystem::directory_iterator(folderPath))
+        {
+            if (entry.is_regular_file())
+            {
+                std::string filename = entry.path().stem().string(); // без расширения
+
+                if (std::find(whitelist.begin(), whitelist.end(), filename) == whitelist.end())
+                {
+                    std::filesystem::remove(entry.path());
+                }
+            }
+        }
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << "[AutoClearMaps v1.0.0] Ошибка при удалении карт: " << ex.what() << std::endl;
+    }
+}
+
+///////////////////////////////////////
+const char* AutoMapDelete::GetLicense()
+{
+	return "GPL";
+}
+
+const char* AutoMapDelete::GetVersion()
+{
+	return "1.0";
+}
+
+const char* AutoMapDelete::GetDate()
+{
+	return __DATE__;
+}
+
+const char *AutoMapDelete::GetLogTag()
+{
+	return "AutoMapDelete";
+}
+
+const char* AutoMapDelete::GetAuthor()
+{
+	return "Pisex";
+}
+
+const char* AutoMapDelete::GetDescription()
+{
+	return "AutoMapDelete";
+}
+
+const char* AutoMapDelete::GetName()
+{
+	return "Auto Map Delete";
+}
+
+const char* AutoMapDelete::GetURL()
+{
+	return "https://discord.gg/g798xERK5Y";
+}
---
+++ auto_map_delete.h
@@ -0,0 +1,31 @@
+﻿#ifndef _INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
+#define _INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
+
+#include <ISmmPlugin.h>
+#include <sh_vector.h>
+#include "utlvector.h"
+#include "ehandle.h"
+#include <iserver.h>
+#include "vector.h"
+#include <deque>
+#include <functional>
+#include <utlstring.h>
+
+class AutoMapDelete final : public ISmmPlugin, public IMetamodListener
+{
+public:
+	bool Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late);
+	bool Unload(char* error, size_t maxlen);
+	void DeleteMaps(const std::string& folderPath, const std::vector<std::string>& whitelist);
+private:
+	const char* GetAuthor();
+	const char* GetName();
+	const char* GetDescription();
+	const char* GetURL();
+	const char* GetLicense();
+	const char* GetVersion();
+	const char* GetDate();
+	const char* GetLogTag();
+};
+
+#endif //_INCLUDE_METAMOD_SOURCE_STUB_PLUGIN_H_
